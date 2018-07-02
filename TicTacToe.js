881ea7558b830678bc8cccfe8a4956ed252ed779
Chase McDowell <nextechcatapult@Chases-Air.localdomain>
2018-07-02 09:58:09 -0400

    Updated

---
+++ TicTacToe.js
@@ -0,0 +1,26 @@
+var currentplayer = "X";
+var nextplayer = "O";
+
+var playerXSelections = NewArray()
+var playerOSelections = NewArray()
+
+const winningCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ]
+  handleClick = function(event) {
+    var cell = event.target
+    console.log(cell.id);
+  }
+  
+  var cells = document.querySelectorAll("td");
+  
+  for(var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', handleClick)
+  }
